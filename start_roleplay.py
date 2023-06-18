d798be836bab2a79e708c47cf5ffae4a59d17cbd
Maximilian-Winter <maximilian.winter.91@gmail.com>
2023-06-18 02:15:44 +0200

    Fixed incoherent writing

--- start_roleplay.py
+++ start_roleplay.py
@@ -17,10 +17,11 @@ class LlamaSettings:
 settings = LlamaSettings()
 settings.model = "../../ggml-v3-models/Dans-PersonalityEngine-30b-ggml-q5_0.bin"
 settings.n_batch = 512
-settings.n_gpu_layers = 7
+settings.n_gpu_layers = 4
 settings.n_threads = 12
 settings.last_n_tokens_size = 2048
 settings.verbose = False
+settings.embedding = True
 
 settings_summarizer = LlamaSettings()
 settings_summarizer.model = "../../ggml-v3-models/WizardLM-7B-uncensored.ggmlv3.q5_1.bin"
@@ -44,21 +45,21 @@ main_model = Llama(
     verbose=settings.verbose
 )
 
-#summarizer_model = Llama(
-#    settings_summarizer.model,
-#    n_gpu_layers=settings_summarizer.n_gpu_layers,
-#    f16_kv=settings_summarizer.f16_kv,
-#    use_mlock=settings_summarizer.use_mlock,
-#    embedding=settings_summarizer.embedding,
-#    n_threads=settings_summarizer.n_threads,
-#    n_batch=settings_summarizer.n_batch,
-#    n_ctx=settings_summarizer.n_ctx,
-#    last_n_tokens_size=settings_summarizer.last_n_tokens_size,
-#    verbose=settings_summarizer.verbose
-#)
+summarizer_model = Llama(
+    settings_summarizer.model,
+    n_gpu_layers=settings_summarizer.n_gpu_layers,
+    f16_kv=settings_summarizer.f16_kv,
+    use_mlock=settings_summarizer.use_mlock,
+    embedding=settings_summarizer.embedding,
+    n_threads=settings_summarizer.n_threads,
+    n_batch=settings_summarizer.n_batch,
+    n_ctx=settings_summarizer.n_ctx,
+    last_n_tokens_size=settings_summarizer.last_n_tokens_size,
+    verbose=settings_summarizer.verbose
+)
 
 
-def main_generate_function(prompt: str = "", max_tokens: int = 500, temperature: float = 0.3,
+def main_generate_function(prompt: str = "", max_tokens: int = 500, temperature: float = 0.6,
                            top_k: int = 40, top_p: float = 0.75, repeat_penalty: float = 1.2, stream: bool = True):
     if character.debug_output:
         print(prompt)
@@ -112,7 +113,7 @@ def summarizer_generate_function(prompt: str = "", max_tokens: int = 200, temper
 
 
 template_summary = """### Instruction:
-Generate a short summary of the following partial dialogue as a long term memory! Pay close attention to not mix up any of the details! Don't make assumptions!
+Generate a one sentence summary of the following partial dialogue as a long term memory!
 ### Input:
 Dialogue:
 {history}
@@ -120,7 +121,7 @@ Dialogue:
 Summary:"""
 
 template_summary_parts = """### Instruction:
-Generate a short summary of the following partial memories as a long term memory! Pay close attention to not mix up any of the details! Don't make assumptions!
+Generate a one sentence summary of the following partial memories as a long term memory!
 ### Input:
 Memories:
 {history}
@@ -176,7 +177,7 @@ character = """Richard Feynman, the charismatic and influential theoretical phys
 character = AICharacter(main_generate_function=main_generate_function,
                         summarizer_generate_function=summarizer_generate_function,
                         tokenizer_encode_function=main_model.tokenizer().encode,
-                        embedding_function=main_model.embed,
+                        embedding_function=summarizer_model.embed,
                         character_name="Richard Feynman", user_name="Maximilian Winter",
                         system_message="Adopt the personality described in the character section below and respond to the current user's message in input. Consider the complete conversation history, the additional context, the character's current location, situation, emotional state, and goals below when writing a response. Ensure that the response is coherent, relevant, and concise. Avoid including any information that is not based on the given context.",
                         scenario="It's 1965, and Feynman has just won the Nobel Prize in Physics, bringing with it a wave of attention and expectations.",
@@ -189,7 +190,7 @@ character = AICharacter(main_generate_function=main_generate_function,
                         summarizer_template=template_summary,
                         summarizer_summaries_template=template_summary_parts,
                         command_registry=command_registry,
-                        max_output_length=500, manual_summarize=True, debug_output=True)
+                        max_output_length=500, manual_summarize=False, debug_output=True)
 
 character.init_chat()
 
